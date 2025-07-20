b11a2962118aaed0c4feac709bf2d991fc72bb22
Atul Rawat <dr6217811@gmail.com>
2025-07-20 20:25:11 +0530

    minor changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -15,7 +15,7 @@ const userAuth = async (req, res, next) => {
         const user = await User.findById(_id);
 
         if (!user) {
-            throw new Error("User not found!");
+            throw new Error("User Not Found!");
         }
         req.user = user;
         next();
